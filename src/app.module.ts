966ef0d8628ae0c8e401295a691c8a593dc8b7c3
sun <thebthebtheb1234@gmail.com>
2023-11-06 17:57:33 +0900

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { ChulModule } from './chul/chul.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabasekimchiModule } from './databasekimchi/databasekimchi.module';
+import Joi from '@hapi/joi';
 
 @Module({
-  imports: [ProductModule, ChulModule],
+  imports : [
+    ConfigModule.forRoot({
+
+      validationSchema : Joi.object({
+
+        POSTGRES_HOST : Joi.string().required(),
+        POSTGRES_PORT : Joi.number().required(),
+        POSTGRES_USER : Joi.string().required(),
+        POSTGRES_PASSWORD : Joi.string().required(),
+        POSTGRES_DB : Joi.string().required(),
+
+      })
+
+    }),
+    ProductModule,
+    DatabasekimchiModule],
+
+
   controllers: [AppController],
   providers: [AppService],
 })
